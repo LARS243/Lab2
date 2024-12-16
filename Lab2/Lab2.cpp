15a9502b24d1d69e5f74dac731e110c367cd4440
LARS243 <arsenka.243@mail.ru>
2024-12-16 15:08:45 +0300

    Добавьте файлы проекта.

---
+++ Lab2/Lab2.cpp
@@ -0,0 +1,15 @@
+﻿#include <iostream>
+#include <string>
+#include <fstream>
+using namespace std;
+
+int main()
+{
+    setlocale(LC_ALL, "RU");
+    std::string message;
+    std::ifstream file;       // поток для чтения
+    file.open("address_o.csv"); // окрываем файл для чтения
+    for (file >> message; !file.eof(); file >> message) {
+        cout << message << "\n";
+    }
+}
