5b0d23a240aeb65996638ecf29e1ab5720160850
LARS243 <arsenka.243@mail.ru>
2024-12-16 15:23:34 +0300

    utf-8

--- Lab2/Lab2.cpp
+++ Lab2/Lab2.cpp
@@ -5,7 +5,7 @@ using namespace std;
 
 int main()
 {
-    setlocale(LC_ALL, "RU");
+    setlocale(LC_ALL, "ru_RU.utf8");
     std::string message;
     std::ifstream file;       // поток для чтения
     file.open("address_o.csv"); // окрываем файл для чтения
