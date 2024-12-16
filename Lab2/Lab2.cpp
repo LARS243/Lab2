a01566e3b168a2e71d1d52815c6d3a55f9a95025
Oleg <borisovos@yandex.ru>
2024-12-16 19:45:37 +0300

    Перестройка

--- Lab2/Lab2.cpp
+++ Lab2/Lab2.cpp
@@ -4,71 +4,223 @@
 #include <vector>
 using namespace std;
 //s
-class list
+class List
 {
 private:
-    class data_of_list
+    class list_of_towns
     {
-    public:
-        string town;
-        string street;
-        string number_of_house;
-        string type_of_house;
-        data_of_list* next_data;
-        int counter;
-        data_of_list(string& town_for_write, string& street_for_write, string& number_of_houses_for_write, string& type_of_houses_for_write)
+    private:
+        class list_of_first_symbol
         {
-            town = town_for_write;
-            street = street_for_write;
-            number_of_house = number_of_houses_for_write;
-            type_of_house = type_of_houses_for_write;
-            next_data = nullptr;
-            counter = 0;
-        }
-    };
-    data_of_list* first_element;
-public:
-    list()
-    {
-        first_element = nullptr;
-    }
-    void insert_town(string& town, string& street, string& number_of_house, string& type_of_house)
-    {
-        if (first_element == nullptr)
-        {
-            data_of_list* start = new data_of_list(town, street, number_of_house, type_of_house);
-            first_element = start;
-        }
-        else
-        {
-            data_of_list* start = first_element;
-            while (start->next_data != nullptr)
+        private:
+            class list_of_streets
             {
-                if (start->town == town and start->street == street and start->type_of_house == type_of_house and start->town == town)
+            private:
+                class list_of_type_homes
                 {
-                    start->counter += 1;
-                    return;
+                public:
+                    string count;
+                    string type;
+                    int counters;
+                    list_of_type_homes* next_type;
+                    list_of_type_homes(string& new_type, string& new_count)
+                    {
+                        counters = 0;
+                        count = new_count;
+                        type = new_type;
+                        next_type = nullptr;
+                    }
+                };
+                list_of_type_homes* first_type;
+            public:
+                string street;
+                list_of_streets* next_street;
+                list_of_streets(string& new_street)
+                {
+                    street = new_street;
+                    first_type = nullptr;
+                    next_street = nullptr;
+                }
+
+                void print_all_types()
+                {
+                    list_of_type_homes* start = first_type;
+                    cout << start->type << "|" << start->count << "|" << "request: " << start->counters << endl;
+                }
+
+                void insert_new_type(string& new_type, string& new_home)
+                {
+                    if (first_type == nullptr)
+                    {
+                        list_of_type_homes* start = new list_of_type_homes(new_type, new_home);
+                        first_type = start;
+                    }
+                    else
+                    {
+                        list_of_type_homes* start = first_type;
+                        while (start->next_type != nullptr and start->type != new_type)
+                        {
+                            start = start->next_type;
+                        }
+                        if (start->type == new_type)
+                        {
+                            start->counters += 1;
+                        }
+                        else
+                        {
+                            list_of_type_homes* real_new_street = new list_of_type_homes(new_type, new_home);
+                            start->next_type = real_new_street;
+                        }
+                    }
+                }
+            };
+            list_of_streets* first_street;
+        public:
+            char symbol;
+            list_of_first_symbol* next_symbol;
+            list_of_first_symbol(char& new_symbol)
+            {
+                first_street = nullptr;
+                symbol = new_symbol;
+                next_symbol = nullptr;
+            }
+
+            void print_all_streets()
+            {
+                list_of_streets* start = first_street;
+                while (start != nullptr)
+                {
+                    cout << start->street << "|";
+                    start->print_all_types();
+                    cout << endl;
+                    start = start->next_street;
+                }
+            }
+
+            void insert_new_street(string& new_street, string& new_type, string& new_home)
+            {
+                if (first_street == nullptr)
+                {
+                    list_of_streets* start = new list_of_streets(new_street);
+                    first_street = start;
+                    start->insert_new_type(new_type, new_home);
                 }
                 else
                 {
-                    start = start->next_data;
+                    list_of_streets* start = first_street;
+                    while (start->next_street != nullptr and start->street != new_street)
+                    {
+                        start = start->next_street;
+                    }
+                    if (start->street == new_street)
+                    {
+                        start->insert_new_type(new_type, new_home);
+                    }
+                    else
+                    {
+                        list_of_streets* real_new_street = new list_of_streets(new_street);
+                        start->next_street = real_new_street;
+                        real_new_street->insert_new_type(new_type, new_home);
+                    }
                 }
             }
-            data_of_list* new_town = new data_of_list(town, street, number_of_house, type_of_house);
-            start->next_data = new_town;
+        };
+        list_of_first_symbol* first_symbol;
+    public:
+        string town;
+        list_of_towns* next_town;
+        list_of_towns(string& new_town)
+        {
+            first_symbol = nullptr;
+            town = new_town;
+            next_town = nullptr;
         }
+
+        void print_all_symbols()
+        {
+            list_of_first_symbol* start = first_symbol;
+            while (start != nullptr)
+            {
+                cout << start->symbol << "|";
+                start->print_all_streets();
+                cout << endl;
+                start = start->next_symbol;
+            }
+        }
+
+        void insert_new_symbol(string& new_street, string& new_type, string& new_home)
+        {
+            char new_symbol = new_street.at(1);
+            if (first_symbol == nullptr)
+            {
+                list_of_first_symbol* start = new list_of_first_symbol(new_symbol);
+                first_symbol = start;
+                start->insert_new_street(new_street, new_type, new_home);
+            }
+            else
+            {
+                list_of_first_symbol* start = first_symbol;
+                while (start->next_symbol != nullptr and start->symbol != new_symbol)
+                {
+                    start = start->next_symbol;
+                }
+                if (start->symbol == new_symbol)
+                {
+                    start->insert_new_street(new_street, new_type, new_home);
+                }
+                else
+                {
+                    list_of_first_symbol* real_new_symbol = new list_of_first_symbol(new_symbol);
+                    start->next_symbol = real_new_symbol;
+                    real_new_symbol->insert_new_street(new_street, new_type, new_home);
+                }
+            }
+        }
+    };
+    list_of_towns* first_town;
+public:
+    List()
+    {
+        first_town = nullptr;
     }
+
     void print_all_list()
     {
-        data_of_list* start = first_element;
-        while (start->next_data != nullptr)
+        list_of_towns* start = first_town;
+        while (start != nullptr)
         {
-            if (start->counter > 0)
+            cout << start->town << "|";
+            start->print_all_symbols();
+            cout << endl;
+            start = start->next_town;
+        }
+    }
+
+    void insert_new_town(string& new_town, string& new_street, string& new_type, string& new_home)
+    {
+        if (first_town == nullptr)
+        {
+            list_of_towns* start = new list_of_towns(new_town);
+            first_town = start;
+            start->insert_new_symbol(new_street, new_type, new_home);
+        }
+        else
+        {
+            list_of_towns* start = first_town;
+            while (start->next_town != nullptr and start->town != new_town)
             {
-                cout << start->town << "|" << start->street << "|" << start->number_of_house << "|" 
-                    << start->type_of_house << "| repeats: " << start->counter+1 << endl;
+                start = start->next_town;
             }
-            start = start->next_data;
+            if (start->town == new_town)
+            {
+                start->insert_new_symbol(new_street, new_type, new_home);
+            }
+            else
+            {
+                list_of_towns* real_new_town = new list_of_towns(new_town);
+                start->next_town = real_new_town;
+                real_new_town->insert_new_symbol(new_street, new_type, new_home);
+            }
         }
     }
 };
@@ -94,7 +246,7 @@ vector<string> string_to_vector(string& message) {
 
 int main()
 {
-    list spisok;
+    List spisok;
     setlocale(LC_ALL, "ru_RU.utf8");
     string message;
     vector<string> matrix;
@@ -105,7 +257,7 @@ int main()
         while (getline(file, message))
         {
             matrix = string_to_vector(message);
-            spisok.insert_town(matrix[0], matrix[1], matrix[2], matrix[3]);
+            spisok.insert_new_town(matrix[0], matrix[1], matrix[2], matrix[3]);
         }
     }
     file.close();
